b33e96b5de18aca8509559600605fb2f37e1fee4
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-08-05 17:34:19 -0400

    Finished most of it

--- js/index.js
+++ js/index.js
@@ -1,39 +1,30 @@
 // Your code goes here
-const navColor = document.querySelectorAll('.nav-link');
 
-/*
-navColor.onclick = function(event) {
-    event.target.style.color = 'blue';
-    console.log('working');
-}*/
-console.log(navColor);
+// loads a console log when the page is loaded up
+document.addEventListener('load', console.log('The page as loaded'));
 
-navColor.forEach(function(element) {
+
+const navUnderline = document.querySelectorAll('.nav-link');
+console.log(navUnderline);
+
+// added underline and prevented default
+navUnderline.forEach(function(element) {
     element.onclick = function(event) {
         event.target.style.textDecoration = 'underline';
         console.log('underlined nav');
+
+        event.preventDefault();
     }
 });
 
 const funZone = document.querySelector('h1');
 console.log(funZone);
 
+// changes the target to a different color
 funZone.addEventListener('mouseover', function(event) {
     event.target.style.color = 'blue';
     console.log('title is blue');
 });
-
-//const allElements = document.all;
-//console.log(allElements);
-
-/*
-allElements.forEach(function(element) {
-    element.keydown = function(event) {
-        if (event.key === 'Escape') {
-            event.target.removeEventListener();
-        }
-    }
-});*/
 
 const imageHeights = document.querySelectorAll('img');
 console.log(imageHeights);
@@ -45,8 +36,28 @@ imageHeights.forEach(function(element) {
     }
 });
 
+// console logs when a key is pressed down 
 document.addEventListener('keydown', event => {
     if (event.key === 'Escape') {
         console.log('keydown works');
     }
+});
+
+
+Array.from(document.all).forEach(function(element) {
+    element.addEventListener('click', function(event) {
+        console.log('Testing for propagation');
+    });
+});
+
+
+const allParagraphs = document.querySelectorAll('p');
+console.log(allParagraphs);
+
+allParagraphs.forEach(function(element) {
+    element.addEventListener('click', function(event) {
+        console.log('This should not propagate');
+
+        event.stopPropagation();
+    });
 });
